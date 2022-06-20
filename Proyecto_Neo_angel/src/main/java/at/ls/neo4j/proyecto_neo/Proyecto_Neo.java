5f8e251ebfe478ed9b9eaebcc10a74c10afd793f
Angel4501 <adponce@unitec.edu>
2022-06-19 19:08:14 -0600

    Tecnologias pdf de los devs finalizado

--- Proyecto_Neo_angel/src/main/java/at/ls/neo4j/proyecto_neo/Proyecto_Neo.java
+++ Proyecto_Neo_angel/src/main/java/at/ls/neo4j/proyecto_neo/Proyecto_Neo.java
@@ -3,29 +3,37 @@ package at.ls.neo4j.proyecto_neo;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
-
-
 /*import org.neo4j.driver.AuthTokens;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.GraphDatabase;
 import org.neo4j.driver.Result;
 import org.neo4j.driver.Session;
 import static org.neo4j.driver.Values.parameters;*/
-
 public class Proyecto_Neo {
-    
-    
+
     public static void main(String[] args) {
         //Web;Machete; Frameworks ;Manejo de blockchain ;Machine learning 
-        String x = "Web;Machete";
+
+        /*String t = "Manejo de frameworks, tambien puedo usar angular y react";
+        String array2[] = t.split("\\r?\\n");
+        String concat2="";
+        for (int i = 0; i < array2.length; i++) {
+            if (i != array2.length - 1) {
+                concat2 += array2[i] + ";";
+            } else {
+                concat2+= array2[i];
+            }
+        }
+        System.out.println(concat2+" length: "+array2.length);*/
+
+        String x = "Web";
         String array[] = x.split(";");
-        String concat="";
-        for(int i=0; i<array.length; i++){
-            concat+=array[i]+"\n";
+        String concat = "";
+        for (int i = 0; i < array.length; i++) {
+            concat += array[i] + "\n";
         }
         System.out.println(concat);
-         
+
         /*int codigo=5;
         String x = "Este es el código "+codigo+" que nos asignaron";
         System.out.println(x);
@@ -34,23 +42,18 @@ public class Proyecto_Neo {
 
         String t = new SimpleDateFormat("dd-MM-yyyy").format(myDate);
         System.out.println(t);*/
-
-        
-        
-       /* Driver driver = GraphDatabase.driver("bolt://localhost:7687",
+ /* Driver driver = GraphDatabase.driver("bolt://localhost:7687",
                 AuthTokens.basic("neo4j", "1234"));
        Persona p = new Persona("Daniel", "Sagastume", 21);
         
         try(Session session = driver.session()){   */
-
-            //Adicionando uma pessoa
-            /*Result result = session.run("CREATE (p:Persona{nombre:$nombre, apellido:$apellido, edad:$edad})",
+        //Adicionando uma pessoa
+        /*Result result = session.run("CREATE (p:Persona{nombre:$nombre, apellido:$apellido, edad:$edad})",
                     parameters("nombre", p.getNombre(), "apellido", p.getApellido(),
                             "edad", p.getEdad()));
             
             System.out.println(result.consume().counters().nodesCreated());*/
-            
-            /*
+ /*
             Result result = session.run("MATCH (p1:Persona{nombre:$nombre}),(p2:Persona{nombre:$nombre2})" +
                     "CREATE (p1)-[:AMIGO]->(p2)",
                     parameters("nombre", "Angel", "nombre2", "Daniel"));
@@ -59,6 +62,6 @@ public class Proyecto_Neo {
         }finally {
             driver.close();
         }
-        */
+         */
     }
 }
